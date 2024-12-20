3eb2c07073a5f42d7aaa26b5596febcb0548c817
Ebad-ur-Rehman <Ebad-ur-Rehman@mx>
2024-12-20 19:59:06 +0500

    change api key option added

---
+++ pages/Check credits.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from utils.account_settings import set_api_key
+
+st.header('💳Check Credits📊')
+# Function to get OpenAI credits
+def show_openai_credits():
+    url = 'https://platform.openai.com/settings/organization/usage'
+    st.info(f'Check your remaining credits at {url}')
+
+
+def change_api_key():
+    api_key = st.text_input('Enter new API key here')
+
+    set_api_key(api_key=api_key)
+
+
+show_openai_credits()
+change_api_key()
